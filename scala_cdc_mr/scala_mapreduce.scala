ac3609a89be85c84748e9cf46aaf4d395695464c
Tomas Vykruta <tomasv@gmail.com>
2014-07-27 13:05:40 -0400

    Added scala mapreduce test project.

---
+++ scala_cdc_mr/scala_mapreduce.scala
@@ -0,0 +1,24 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.regression.LabeledPoint
+
+// Load and parse the data file
+val data = sc.textFile("gs://bucket-tvykruta/datasets/natality_features/*")
+val parsedData = data.map { line =>
+  val parts = line.split(',')
+  LabeledPoint(parts(0).toDouble, parts.tail.map(x => x.toDouble).toArray)
+}
+
+// Run training algorithm to build the model
+val numIterations = 60
+val model = SVMWithSGD.train(parsedData, numIterations, 0.025, .1, 1.0,
+    Array[Double](1, 1, 1, 1, 1, 1, 1))
+
+// Evaluate model on training examples and compute training error
+val labelAndPreds = parsedData.map { point =>
+  val prediction = model.predict(point.features)
+  (point.label, prediction)
+}
+val trainErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / parsedData.count
+println("Training Error = " + trainErr)
+
