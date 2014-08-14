5c0ea84cfc219f91f223f917ff912b158652cd80
Tom Vykruat <tomasv@gmail.com>
2014-08-14 01:26:56 +0000

    XML java parser build file now uses env vars.

--- src/hadoop2/java/wordcount_xml/build.sh
+++ src/hadoop2/java/wordcount_xml/build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-HADOOP_PATH="/home/hadoop/hadoop-install/share/hadoop"
-
+HADOOP_PATH="$HADOOP_COMMON_HOME/share/hadoop"
 INCLUDES="$HADOOP_PATH/common/hadoop-common-2.2.0.jar"
 INCLUDES="$INCLUDES:$HADOOP_PATH/mapreduce/hadoop-mapreduce-client-core-2.2.0.jar"
 INCLUDES="$INCLUDES:$HADOOP_PATH/mapreduce/lib/hadoop-annotations-2.2.0.jar"
 INCLUDES="$INCLUDES:./jdom/build/jdom-1.1.3.jar"
 
-INCLUDES=$HADOOP_PATH/common/hadoop-common-2.2.0.jar:$HADOOP_PATH/mapreduce/hadoop-mapreduce-client-core-2.2.0.jar:$HADOOP_PATH/mapreduce/lib/hadoop-annotations-2.2.0.jar 
 javac -classpath $INCLUDES -d wordcount_classes WordCount.java
 jar -cvf wordcount.jar -C wordcount_classes/ .
