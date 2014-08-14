8deff5d020cb1ed676dcfdc46176f1d4e2a0a5e7
Tom Vykruat <tomasv@gmail.com>
2014-08-14 16:29:53 +0000

    Added ignore file.'

--- src/hadoop2/java/xmlparser2/exec_hadoop.sh
+++ src/hadoop2/java/xmlparser2/exec_hadoop.sh
@@ -1,1 +1,1 @@
-hadoop jar xmlinputformat.jar ParserDriver gs://bucket-tvykruta/lidc/LIDC-IDRI-0001/*/*/*.xml gs://bucket-tvykruta/output5
+hadoop jar xmlinputformat.jar XmlDriver gs://bucket-tvykruta/lidc/LIDC-IDRI-0001/*/*/*.xml gs://bucket-tvykruta/output5
