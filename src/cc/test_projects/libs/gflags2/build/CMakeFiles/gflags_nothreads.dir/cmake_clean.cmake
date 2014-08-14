73f4d66eb23b5b4aedca5be6207ce3c2c114db14
Tom Vykruat <tomasv@gmail.com>
2014-08-14 01:09:08 +0000

    Fixed gflags

--- src/cc/test_projects/libs/gflags2/build/CMakeFiles/gflags_nothreads.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/gflags_nothreads"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/gflags_nothreads.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
