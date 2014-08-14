bf21e4daafa187d99f630d1500928e66f863e23d
Tom Vykruat <tomasv@gmail.com>
2014-08-14 01:16:30 +0000

    Readded gflags.

---
+++ src/cc/test_projects/libs/gflags/build/CMakeFiles/gflags.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gflags"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/gflags.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
